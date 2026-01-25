f5b3a075d2a0d7654e224ecee441b99cddae4f24
tennyhu <tennyhu@hotmail.com>
2026-01-25 14:51:28 -0500

    starter files basics complete

---
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.app.todoapp.controller;
+
+import com.app.todoapp.models.Task;
+import com.app.todoapp.services.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/todotasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/todotasks";
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return ResponseEntity.ok().build();
+    }
+}
